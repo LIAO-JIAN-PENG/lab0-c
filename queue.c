231f8570036a92bbcce41418211f485028879e16
Jpeng <alan36257407@gmail.com>
2024-03-01 16:45:37 +0800

    Implement q_sort function using quick sort
    Implement q_sort function to sort elements of a queue in ascending or
    descending order. The function utilizes a divide-and-conquer approach,
    recursively sorting smaller sublists and merging them back together.
    The pivot element is chosen from the initial list, and elements are
    moved to separate lists based on whether they are greater or less than
    the pivot. Finally, the sorted lists are merged back into the original
    queue.

--- queue.c
+++ queue.c
@@ -217,7 +217,42 @@ void q_reverseK(struct list_head *head, int k)
 }
 
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend)
+{
+    struct list_head list_less, list_greater;
+    element_t *pivot;
+    element_t *item = NULL, *is = NULL;
+
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    INIT_LIST_HEAD(&list_less);
+    INIT_LIST_HEAD(&list_greater);
+
+    pivot = list_first_entry(head, element_t, list);
+    list_del(&pivot->list);
+
+    list_for_each_entry_safe (item, is, head, list) {
+        if (strcmp(item->value, pivot->value) < 0)
+            list_move_tail(&item->list, &list_less);
+        else
+            list_move_tail(&item->list, &list_greater);
+    }
+
+    q_sort(&list_less, descend);
+    q_sort(&list_greater, descend);
+
+
+    list_add(&pivot->list, head);
+
+    if (descend) {
+        list_splice(&list_greater, head);
+        list_splice_tail(&list_less, head);
+    } else {
+        list_splice(&list_less, head);
+        list_splice_tail(&list_greater, head);
+    }
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
