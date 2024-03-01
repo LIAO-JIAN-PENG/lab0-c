18f0c5f8d7c9bf8e6ceee911a35cfcd7e7f02f31
Jpeng <alan36257407@gmail.com>
2024-03-01 23:19:59 +0800

    Implement q_ascend and q_descend functions
    These functions facilitate the removal of nodes from a linked list
    based on whether a node with a strictly lesser or greater value,
    respectively, exists to the right side of it. The process involves
    reversing the queue, identifying the maximum or minimum value at each
    node, and subsequently removing nodes with larger or smaller values
    during node traversal.
    
    Here's an example illustrating the q_descend function:
    
    Original Queue: 5 -> 2 -> 13 -> 3 -> 8
    After Reversal: 8 -> 3 -> 13 -> 2 -> 5
    Traversal:
    - Max value: 8, resulting queue: 8 -> 13 -> 2 -> 5
    - Max value: 13, no removals
    - Max value: 13, resulting queue: 8 -> 13 -> 5
    - Max value: 13, no removals
    Final Queue: 13 -> 8

--- queue.c
+++ queue.c
@@ -259,7 +259,24 @@ void q_sort(struct list_head *head, bool descend)
 int q_ascend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    q_reverse(head);
+
+    char *min = NULL;
+    struct list_head *li, *tmp;
+
+    list_for_each_safe (li, tmp, head) {
+        element_t *e = list_entry(li, element_t, list);
+        if (!min || strcmp(e->value, min) < 0)
+            min = e->value;
+        else {
+            list_del(li);
+            free(e->value);
+            free(e);
+        }
+    }
+
+    q_reverse(head);
+    return q_size(head);
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
@@ -267,7 +284,24 @@ int q_ascend(struct list_head *head)
 int q_descend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    q_reverse(head);
+
+    char *max = NULL;
+    struct list_head *li, *tmp;
+
+    list_for_each_safe (li, tmp, head) {
+        element_t *e = list_entry(li, element_t, list);
+        if (!max || strcmp(e->value, max) > 0)
+            max = e->value;
+        else {
+            list_del(li);
+            free(e->value);
+            free(e);
+        }
+    }
+
+    q_reverse(head);
+    return q_size(head);
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
